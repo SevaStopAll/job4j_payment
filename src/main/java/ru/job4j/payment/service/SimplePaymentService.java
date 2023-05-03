7f1906a53066e8de9b37dd8a5ca19590abb45b2e
SevaStopAll <sevastopallrh@yandex.ru>
2023-05-03 13:54:39 +0400

    Change service a little

--- src/main/java/ru/job4j/payment/service/SimplePaymentService.java
+++ src/main/java/ru/job4j/payment/service/SimplePaymentService.java
@@ -1,13 +1,17 @@
 package ru.job4j.payment.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.job4j.payment.domain.Payment;
 import ru.job4j.payment.repository.PayMethodRepository;
 import ru.job4j.payment.repository.PaymentRepository;
 import ru.job4j.payment.repository.StatusRepository;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -33,5 +37,18 @@ public class SimplePaymentService implements PaymentService {
     @Override
     public Collection<Payment> findAll() {
         return payments.findAll();
+    }
+
+    @KafkaListener(topics = "payment_service")
+    public void receiveStatus(Map<String, Integer> data) {
+
+    }
+
+    @Transactional
+    public void changeStatus(Order order) {
+        Map<String, Integer> data = new HashMap();
+        data.put("id", order.getId());
+        data.put("status", order.getStatus().getId());
+        kafkaTemplate.send("cooked_order", data);
     }
 }
